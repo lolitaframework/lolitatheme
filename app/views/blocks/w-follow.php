1b7aaa02634c4213e0abd51607211e6f3b26cb2a
gcofficial <gurievcreative@gmail.com>
2016-11-07 21:46:28 +0200

    Style guid fixes

--- app/views/blocks/w-follow.php
+++ app/views/blocks/w-follow.php
@@ -4,7 +4,7 @@
         <h5 class="w-folow__title"><?php _e('Folow', 'lolita') ?></h5>
         <div class="w-folow__frame">
             <ul class="w-folow__items">
-                <?php foreach ($items['data'] as $i): ?>
+                <?php foreach ($items['data'] as $i) : ?>
                     <li class="w-folow__item">
                         <a href="<?php echo $i['link'] ?>" class="w-folow__item__link">
                             <img class="w-folow__item__image" src="<?php echo $i['images']['thumbnail']['url'] ?>" alt="<?php echo esc_attr($i['caption']['text']) ?>">
