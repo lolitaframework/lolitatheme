67e1a69119c72fd703d163871d0108e36ec69373
vitalik1921 <vitalik1921@gmail.com>
2016-11-17 21:23:23 +0200

    Fixes

--- app/assets/js/w-search-block.js
+++ app/assets/js/w-search-block.js
@@ -1,19 +1,37 @@
-(function($){
-    $w_search_block = $('.w-search-block');
-    $w_search_block__close = $('.w-search-block__close');
+var $w_search_block = jQuery('.w-search-block');
+var $w_search_block__close = jQuery('.w-search-block__close');
 
-    $('body').on('w_menu__link__search_click', 
-        function() {
-            $w_search_block.removeClass('w-search-block--hidden');
-            $w_search_block.addClass('w-search-block--visible');
-        }
-    );
+// add overlay
+var $overlay = jQuery('<div id="overlay"></div>');
+$overlay.css({
+    'display': 'none',
+    'position': 'fixed',
+    'z-index': '9999',
+    'width': '100%',
+    'height': '100%',
+    'background-color': '#000',
+    'opacity': '0.5'
+});
+jQuery('body').prepend($overlay);
 
-    $w_search_block__close.on('click',
-        function() {
-            $w_search_block.addClass('w-search-block--hidden');
-            $w_search_block.removeClass('w-search-block--visible');
-            $('body').trigger('w_search_block__close');
+
+jQuery('body').on('w_menu__link__search_click',
+    function() {
+        $w_search_block.removeClass('w-search-block--hidden');
+        $w_search_block.addClass('w-search-block--visible');
+        if ($w_search_block.hasClass('w-search-block--header')) {
+            $overlay.show();
         }
-    );
-}(jQuery));
+    }
+);
+
+$w_search_block__close.on('click',
+    function() {
+        $w_search_block.addClass('w-search-block--hidden');
+        $w_search_block.removeClass('w-search-block--visible');
+        if ($w_search_block.hasClass('w-search-block--header')) {
+            $overlay.hide();
+        }
+        jQuery('body').trigger('w_search_block__close');
+    }
+);
