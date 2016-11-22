dd9d05bb0f57870f74f810a05d82fc26f6c0ff68
gcofficial <gurievcreative@gmail.com>
2016-11-22 17:26:04 +0200

    google fixing

--- app/assets/css/style.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-type: text/css; charset: UTF-8");
-include 'main.css';
-include 'blocks.common.css';
-include 'customs.css';
-include 'tomorrow-night.min.css';
-include 'libs.css';
