b228d54ce44f0c60c2bcf3e98185bfd6fcb75c13
gcofficial <gurievcreative@gmail.com>
2016-11-22 15:05:21 +0200

    b-post-preview added

---
+++ app/views/blocks/b-post-preview.php
@@ -0,0 +1,29 @@
+<!-- b-post-preview -->
+<article class="b-post-preview">
+    <?php if ($p->img()->isInitialized()) : ?>
+    <a href="<?php echo $p->link() ?>" class="b-post-preview__thumbnail">
+            <img class="b-post-preview__thumbnail__image" src="<?php echo $p->img()->src('200x200') ?>" alt="<?php echo $p->img()->alt() ?>">
+            <span class="b-post-preview__thumbnail__meta">Posted on <?php echo $p->date() ?></span>
+    </a>
+    <?php endif ?>
+    <div class="b-post-preview__content">
+        <h3 class="b-post-preview__content__title"><a href="<?php echo $p->link() ?>" class="b-post-preview__content__title__link"><?php echo $p->title() ?></a></h3>
+        <div class="b-post-preview__content__excerpt">
+        <?php
+            $content = preg_replace(
+                '/<pre class="prettyprint.*/s',
+                '',
+                $p->post_content
+            );
+            $content = str_replace(
+                '<h2>Example usage</h2>',
+                '',
+                $content
+            );
+            echo $content;
+        ?>
+        </div>
+        <a href="<?php echo $p->link() ?>" class="b-post-preview__content__read-more"><?php _e('Read more', 'lolita') ?></a>
+    </div>
+</article>
+<!-- /b-post-preview -->
