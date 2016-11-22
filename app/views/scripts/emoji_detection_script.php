d9e0c354cfe4eff630e1c118c20607e9362dfb96
gcofficial <gurievcreative@gmail.com>
2016-11-22 10:33:25 +0200

    Styleguide fixes

--- app/views/scripts/emoji_detection_script.php
+++ app/views/scripts/emoji_detection_script.php
@@ -1,4 +1,4 @@
 <script type="text/javascript">
-    window._wpemojiSettings = <?php echo wp_json_encode( $settings ); ?>;
-    <?php readfile( ABSPATH . WPINC . "/js/wp-emoji-loader.js" ); ?>
+    window._wpemojiSettings = <?php echo wp_json_encode($settings); ?>;
+    <?php readfile(ABSPATH . WPINC . "/js/wp-emoji-loader.js"); ?>
 </script>
