65864ac3175f6db66e538d732bff07f86fcf4640
gcofficial <gurievcreative@gmail.com>
2016-11-27 21:00:54 +0200

    gif

--- app/views/blocks/w-banner.php
+++ app/views/blocks/w-banner.php
@@ -1,5 +1,8 @@
 <!-- w-banner -->
 <div class="<?php echo $class ?>">
     <div class="w-banner__image" style="background-image: url('<?php echo $img->src('20x20') ?>');" data-large="<?php echo $img->src('1440x500') ?>"></div>
+    <?php echo '<pre>';
+    var_dump($img->mime());
+    echo '</pre>'; ?>
 </div>
 <!-- /w-banner -->
