03f728c75d99ae9a5aa3aff013d3ec46ef852776
Vitaliy Shebela <vitalik.privat@gmail.com>
2016-11-28 11:12:35 +0200

    Refactored

--- app/views/blocks/w-banner.php
+++ app/views/blocks/w-banner.php
@@ -1,6 +1,6 @@
 <!-- w-banner -->
 <div class="<?php echo $class ?>">
-    <?php if ( 'image/gif' !== $img->mime()) : ?>
+    <?php if ('image/gif' !== $img->mime()) : ?>
         <div class="w-banner__image" style="background-image: url('<?php echo $img->src('20x20') ?>');" data-large="<?php echo $img->src('1440x500') ?>"></div>
     <?php else : ?>
         <div class="w-banner__image" style="background-image: url('<?php echo $img->src('20x20') ?>');" data-large="<?php echo $img->src('full') ?>"></div>
